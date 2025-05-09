f905ef646ec28f84199762e2da54e3529103cbee
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-09 17:00:26 +0200

    Fix import alias in urls.py and correct typo in ProductList view reference

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -3,5 +3,5 @@ from .views import ProductList
 
 urlpatterns = [
   path('products/', ProductList.as_view()),
-  path('products/<str:category>/', ProductList.as__view()),
+  path('products/<str:category>/', ProductList.as_view()),
 ]
