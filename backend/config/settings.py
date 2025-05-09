0a4b45f33038ad24b6b44c8eff6bbc31ff41974f
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-09 15:42:55 +0200

    Install and set up RESTApi

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'store',
 ]
 
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media' #TODO: Set up to serve media files in development
+MEDIA_ROOT = BASE_DIR / 'media' 
 
 STATIC_URL = 'static/'
 
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Products
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductList(APIView):
+  def get(self, request, category=None):
+    if category:
+      products = Products.objects.filter(category__iexact=category) #TODO: explain this line of code
+    else:
+      products = Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
+  
