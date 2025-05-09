002295dfbf171f56e75e840ce098a9e1e116604a
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-09 23:37:45 +0200

    Start setting up API call for products

--- louise-pet-accessories/src/pages/ProductsPage.jsx
+++ louise-pet-accessories/src/pages/ProductsPage.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
-import products from "../data/products";
 import ProductCard from '../components/ProductCard';
 
 const ProductPage = () => {
   const { category } = useParams();
-  const filteredProducts = products.filter(product => product.category === category);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   return (
-    <div className="p-6">
+    <div className="p-6"> #TODO: Continue with useEffect setup
       <h2 className="text-3xl text-yellow-300 text-center font-bold mb-5 capitalize">{category}</h2>
       <div className="grid grid-cols-1 sm-grid-cols-2 md:grid-cols-3 gap-6">
         {filteredProducts.map(product => (
