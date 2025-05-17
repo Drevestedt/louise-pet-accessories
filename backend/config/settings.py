acae0a2c3023e2245e681b4ae730adc6716d9379
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-17 20:10:05 +0200

    Fix MEDIA_ROOT definition and clean up URL patterns

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_ROOT = BASE_DIR, 'media'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
