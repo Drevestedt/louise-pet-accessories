d7cc45d4cd4fff5379355ccd426e9ff96a1e31af
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-21 22:10:26 +0200

    Add vite-plugin-singlefile

--- louise-pet-accessories/vite.config.js
+++ louise-pet-accessories/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import singleFile from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), singleFile()],
   base: '/louise-pet-accessories/',
+  build: {
+    assetsInlineLimit: 100000000,
+  }
 })
