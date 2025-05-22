da75ed2aa689eaafc73fd948de9ed982e144e536
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-22 19:10:30 +0200

    Fix import statement for vite-plugin-singlefile in vite.config.js

--- louise-pet-accessories/vite.config.js
+++ louise-pet-accessories/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import singleFile from 'vite-plugin-singlefile';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), singleFile()],
+  plugins: [react(), viteSingleFile()],
   base: '/louise-pet-accessories/',
   build: {
     assetsInlineLimit: 100000000,
